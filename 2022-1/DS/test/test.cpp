95d0630f9a76f19745500fe16bdce49201b2687b
dinleo <dinleo11@gmail.com>
2022-04-03 02:32:39 +0900

    init

--- 2022-1/DS/test/test.cpp
+++ 2022-1/DS/test/test.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 // A function with a global name
-int sqr( int n ) {
-    return n*n;
+int sqr(int n)
+{
+    return n * n;
 }
 
-int main() {
+int main()
+{
     cout << "The square of 3 is " << sqr(3) << endl;
     return 0;
 }
-
