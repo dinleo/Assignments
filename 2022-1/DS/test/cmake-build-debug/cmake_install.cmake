a18a297462a3b2dc3bba7f2de183149d17608195
dinleo <dinleo11@gmail.com>
2022-04-02 21:46:37 +0900

    init

--- 2022-1/DS/test/cmake-build-debug/cmake_install.cmake
+++ 2022-1/DS/test/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/dinle/Code/Assignments/2022-1/DS/test
+# Install script for directory: C:/Users/dinle/Code/Assignments/2022-1/DS/TEST
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/test")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/TEST")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/dinle/Code/Assignments/2022-1/DS/test/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/dinle/Code/Assignments/2022-1/DS/TEST/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
